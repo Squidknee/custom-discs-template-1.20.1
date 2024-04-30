2173e1b62b09abdf9782bc09a2cd2ff2a45f7edd
Squidknee <72271842+Squidknee@users.noreply.github.com>
2024-04-29 21:15:14 -0400

    Initial commit

---
+++ src/main/java/net/syd/customdiscs/CustomDiscsClient.java
@@ -0,0 +1,10 @@
+package net.syd.customdiscs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomDiscsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/syd/customdiscs/CustomDiscsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.syd.customdiscs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomDiscsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
